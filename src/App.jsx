d940b9f7619a4e901f42701ad8228bcd708d04c8
1706Priyanka <singhpriyanka1706@gmail.com>
2025-02-22 13:25:43 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import HeaderComponent from './components/Header'
+
+function App() {
+  return (
+    <>
+    <HeaderComponent />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function HeaderComponent() {
+  return (
+    <header>
+      <h1>My Header</h1>
+    </header>
+  );
+}
+
+export default HeaderComponent;
